55eaf37d19d8902c203645457c988cf0fb45d977
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-08-24 18:48:42 -0400

    comment out non-functional endpoints (will delete once sure they are not used)

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const path = require('path');
 const app = express();
 const PORT = 5711;  // TODO - must make Heroku friendly
 
-// TODO - next two lines added 8/24 9:13 a.m. - let's see if this fixes it
-// TODO - 8/23 8:14 a.m. - YES!!
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -15,11 +13,10 @@ console.log('notesDataObj on load = "'
 
 app.use(express.static('public')); console.log('public set in server - line 9');
 
-app.get('/api/db', (req, res) => {
-  console.log('get /api/db hit in server - line 12');
-  res.json(notesDataObj); 
-
-});
+// app.get('/api/db', (req, res) => {
+//   console.log('get /api/db hit in server - line 12');
+//   res.json(notesDataObj); 
+// });
 
 app.get('/notes', (req, res) => {
     console.log('get /notes hit in server - line 18');
@@ -55,27 +52,24 @@ app.post('/api/notes', (req, res) => {
   } else {
     res.status(400).json('Request body must contain title and text');
   }
-
-  // Log the response body to the console
-  console.log(req.body);
-
 })
 
-app.put('/api/db', (req, res) => {
-  console.log('put /api/db it in server line - 30');
-  res.send("you hit the put endpoint for api/db!"); 
+// app.put('/api/db', (req, res) => {
+//   console.log('put /api/db it in server line - 30');
+//   res.send("you hit the put endpoint for api/db!"); 
 
-});
-app.delete('/api/db', (req, res) => {
-  console.log('delete /api/db hit inserver - line 35');
-  res.send("you hit the delete endpoint for api/db!"); 
+// });
+// app.delete('/api/db', (req, res) => {
+//   console.log('delete /api/db hit inserver - line 35');
+//   res.send("you hit the delete endpoint for api/db!"); 
 
-});
-app.post('/api/db', (req, res) => {
-  console.log('post /api/db hit in server - line 40');
-  res.send("you hit the post endpoint for api/db!"); 
+// });
+// app.post('/api/db', (req, res) => {
+//   console.log('post /api/db hit in server - line 40');
+//   res.send("you hit the post endpoint for api/db!"); 
 
-});
+// });
+
 app.get('*', (req, res) => {
   console.log('get * hit in server - line 46');
   res.sendFile(path.join(__dirname, './public/index.html'));
